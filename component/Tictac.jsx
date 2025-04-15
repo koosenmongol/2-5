f4019487191e9701f0b66c56f93c4fd0edb1ba11
koosenmongol <koosenmongol@gmail.com>
2025-04-15 16:53:15 +0800

    Tictack

--- component/Tictac.jsx
+++ component/Tictac.jsx
@@ -5,7 +5,10 @@ const Tictac = () => {
   const [lastCLick, setLastCLick] = useState("O");
   const [winCheck, setWinCheck] = useState(false);
   const [draw, setDraw] = useState(false);
+  const [player, setPlayer] = useState("");
   const onChange = (index) => {
+    if (winCheck || draw) return;
+
     if (boxText[index] === "") {
       boxText[index] = lastCLick === "X" ? "O" : "X";
       setBoxText([...boxText]);
@@ -28,60 +31,65 @@ const Tictac = () => {
         boxText[0] === boxText[1] &&
         boxText[1] === boxText[2] &&
         boxText[2] !== ""
-      )
+      ) {
+        setPlayer(boxText[0]);
         setWinCheck(true);
-      else if (
+      } else if (
         boxText[3] === boxText[4] &&
         boxText[4] === boxText[5] &&
         boxText[5] !== ""
-      )
+      ) {
+        setPlayer(boxText[3]);
         setWinCheck(true);
-      else if (
+      } else if (
         boxText[6] === boxText[7] &&
         boxText[7] === boxText[8] &&
         boxText[8] !== ""
-      )
+      ) {
+        setPlayer(boxText[6]);
         setWinCheck(true);
-      else if (
+      } else if (
         boxText[0] === boxText[3] &&
         boxText[6] === boxText[3] &&
         boxText[3] !== ""
-      )
+      ) {
+        setPlayer(boxText[0]);
         setWinCheck(true);
-      else if (
+      } else if (
         boxText[1] === boxText[4] &&
         boxText[7] === boxText[4] &&
         boxText[4] !== ""
-      )
+      ) {
+        setPlayer(boxText[1]);
         setWinCheck(true);
-      else if (
+      } else if (
         boxText[2] === boxText[5] &&
         boxText[5] === boxText[8] &&
         boxText[8] !== ""
-      )
+      ) {
+        setPlayer(boxText[5]);
         setWinCheck(true);
-      else if (
+      } else if (
         boxText[0] === boxText[4] &&
         boxText[4] === boxText[8] &&
         boxText[8] !== ""
-      )
+      ) {
+        setPlayer(boxText[2]);
         setWinCheck(true);
-      else if (
+      } else if (
         boxText[2] === boxText[4] &&
         boxText[6] === boxText[4] &&
         boxText[6] !== ""
-      )
+      ) {
+        setPlayer(boxText[2]);
         setWinCheck(true);
+      }
     } else setDraw(true);
   };
   return (
     <div className="container">
-      <h2 style={{ textAlign: "left" }}>
-        Аль тоглогч хожсоныг гаргаж ирэх <br /> Хожсоны дараа дахиж дарагдахгүй
-        байх
-      </h2>
       {draw && <h1>Тэнцлээ</h1>}
-      {winCheck && <h1>Хожлоо</h1>}
+      {winCheck && <h1> {player} хожлоо</h1>}
       {(winCheck || draw) && <button onClick={Resetgame}>Reset game</button>}
       <div className="Tictac">
         {boxText.map((text, index) => (
